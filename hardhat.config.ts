978c7212de13a84575bfafcef5f3d2e1a1376112
Vienio99 <kyve96@gmail.com>
2022-01-04 12:35:48 +0100

    updated NFT contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,19 +1,19 @@
-import { task } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
+import fs from "fs";
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (args, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(await account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+const privateKey = fs.readFileSync(".secret").toString();
 
 export default {
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: "https://fllulozfvc2j.usemoralis.com:2053/server",
+      accounts: [privateKey],
+    },
+    mainnet: {},
+    accounts: [privateKey],
+  },
   solidity: "0.8.4",
 };
